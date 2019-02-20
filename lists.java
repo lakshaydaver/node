fa06ef33bd43012e07bb4a677446c056f6066541
lakshaydaver <lakshay.daver_ccv17@gla.ac.in>
2019-02-20 11:12:51 +0530

    commit

--- lists.java
+++ lists.java
@@ -3,4 +3,27 @@ package list;
 public class lists {
 
     private int data;
+
+    public int getData() {
+        return data;
+    }
+
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
