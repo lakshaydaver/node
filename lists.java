dae5725ffd276e400f0e11d733085b462ce30c38
lakshaydaver <lakshay.daver_ccv17@gla.ac.in>
2019-02-20 11:48:40 +0530

    constructor

--- lists.java
+++ lists.java
@@ -1,5 +1,7 @@
 package list;
 
+import javax.xml.soap.Node;
+
 public class lists {
 
     private int data;
@@ -17,13 +19,35 @@ public class lists {
         this.data = data;
     }
 
+    public Node getNext() {
+        return next;
+    }
 
 
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
 
+public Node(int data)
+{
 
+    this.data=data;
 
+}
+public Node()
+{
 
+    this.data=0;
+    this.nxt=null;
+
+}
+public Node(int data,Node nxt)
+{
+    this.data=data;
+    this.next=next;
+
+}
 
 
 
